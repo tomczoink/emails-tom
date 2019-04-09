4a0ef3f7f431920afd8fd270b464b02cad0f2e82
tomC <tc13270@my.bristol.ac.uk>
2019-04-09 16:28:18 +0100

    Update index.js

--- my-cool-action/index.js
+++ my-cool-action/index.js
@@ -8,9 +8,9 @@ Toolkit.run(async tools => {
   //console.log(tools.context.payload.pull_request)
   let num = tools.context.payload.number;
   console.log(tools.context.payload);
-  console.log(tools.context.payload.number);
+  console.log(num);
 
-  //const list = await tools.github.pulls.listFiles('tomczoink', 'emails-tom', num);
-  //console.log(list);
+  const list = await tools.github.pulls.listFiles({'tomczoink', 'emails-tom', num});
+  console.log(list);
   tools.exit.success('We did it!')
 })
