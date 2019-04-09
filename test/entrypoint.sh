3c7d5b1f56b617551de97876d028c9af32fe4263
tomC <tc13270@my.bristol.ac.uk>
2019-04-09 13:50:52 +0100

    Update entrypoint.sh

--- test/entrypoint.sh
+++ test/entrypoint.sh
@@ -1,5 +1,7 @@
 npm install
-for file in product-updates/*.mjml; do 
+bin/mjml-compile "product-updates/18-12-20-OpenDataStreamingProgram/odsp-email.mjml"
+
+for file in product-updates/*/index.mjml; do 
     if [ -f "$file" ]; then 
       bin/mjml-compile "$file" 
     fi 
