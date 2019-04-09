116b3657e4711263ceade247029a9064e8c9edf2
tomC <tc13270@my.bristol.ac.uk>
2019-04-09 10:47:13 +0100

    Update run-tests.sh

--- .github/run-tests.sh
+++ .github/run-tests.sh
@@ -1,5 +1,4 @@
-sudo PATH="$PATH:/usr/local/bin" npm install -g grunt-cli
-apt update
-apt install nodejs npm
+sudo apt-get install nodejs-legacy
+apt install npm
 npm install
 bin/mjml-compile "product-updates/18-12-20-OpenDataStreamingProgram/odsp-email.mjml"
